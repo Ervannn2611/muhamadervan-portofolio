721f6609e5ed869a18cdc7668764492d2d12c593
Ervannn2611 <muhamadervan@smkwikrama.sch.id>
2025-06-04 11:55:11 +0700

    feat: setup initial project structure

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,8 @@
 'use strict';
 
-
-
 /**
  * navbar toggle
  */
-
 const header = document.querySelector("[data-header]");
 const navToggleBtn = document.querySelector("[data-nav-toggle-btn]");
 
@@ -17,7 +14,6 @@ navToggleBtn.addEventListener("click", function () {
 /**
  * toggle the navbar when click any navbar link
  */
-
 const navbarLinks = document.querySelectorAll("[data-nav-link]");
 
 for (let i = 0; i < navbarLinks.length; i++) {
@@ -27,14 +23,9 @@ for (let i = 0; i < navbarLinks.length; i++) {
   });
 }
 
-
-
-
-
 /**
  * back to top & header
  */
-
 const backTopBtn = document.querySelector("[data-back-to-top]");
 
 window.addEventListener("scroll", function () {
@@ -46,3 +37,42 @@ window.addEventListener("scroll", function () {
     backTopBtn.classList.remove("active");
   }
 });
+
+/**
+ * EmailJS Send Form
+ */
+document.addEventListener("DOMContentLoaded", function () {
+  emailjs.init("uppODNWqjBnJ2R7wC"); // Ganti dengan User ID kamu
+
+  const contactForm = document.getElementById("contact-form");
+
+  if (contactForm) {
+    contactForm.addEventListener("submit", function (event) {
+      event.preventDefault();
+
+      emailjs.sendForm("service_gss9858", "template_88dnk4p", contactForm)
+        .then(function () {
+          Swal.fire({
+            icon: 'success',
+            title: 'Berhasil!',
+            text: 'Pesan berhasil dikirim. Terima kasih!',
+            confirmButtonColor: '#3085d6'
+          });
+          contactForm.reset();
+        }, function (error) {
+          Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Gagal mengirim pesan: ' + error.text,
+            confirmButtonColor: '#d33'
+          });
+        });
+    });
+  } else {
+    console.error("Form dengan ID 'contact-form' tidak ditemukan.");
+  }
+});
+/**
+ * Preloader
+ */
+
